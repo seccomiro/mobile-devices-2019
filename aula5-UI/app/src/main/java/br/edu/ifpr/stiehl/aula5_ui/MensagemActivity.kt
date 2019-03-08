3a3485e1831ba3732adb57ac1acb47d9aae8f488
Diego Stiehl <diego.stiehl@ifpr.edu.br>
2019-03-08 10:13:06 -0300

    Adicionando Aula 5 - UI

---
+++ aula5-UI/app/src/main/java/br/edu/ifpr/stiehl/aula5_ui/MensagemActivity.kt
@@ -0,0 +1,14 @@
+package br.edu.ifpr.stiehl.aula5_ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MensagemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensagem)
+
+//        val mensagem = getString(R.string.message)
+    }
+}
